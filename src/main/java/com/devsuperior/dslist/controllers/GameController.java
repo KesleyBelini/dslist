f56c055422229fe54afca209949f62ec7dc09526
Kesley Belini <kesley.beline@gmail.com>
2025-08-19 17:39:39 -0300

    feat: adiciona endpoint findById no GameController

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -2,7 +2,9 @@ package com.devsuperior.dslist.controllers;
 
 import java.util.List;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,11 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll() {
         return gameService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        return gameService.findById(id);
     }
 
 }
