05a98a33398cfa892334a372fb837a743365b912
Kesley Belini <kesley.beline@gmail.com>
2025-09-02 23:04:17 -0300

    feat(data): add repositories with custom queries and JPA projections

--- src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -1,13 +1,16 @@
 package com.devsuperior.dslist.projections;
 
-import com.devsuperior.dslist.entities.Game;
-
 public interface GameMinProjection {
 
     Long getId();
+
     String getTitle();
+
     Integer getYear();
+
     String getImgUrl();
+
     String getShortDescription();
+
     Integer getPosition();
 }
